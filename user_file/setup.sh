c9eb4c460074ab90265b5bed1adeed76aa2119df
khoitd1997 <khoidinhtrinh@gmail.com>
2019-03-03 23:41:07 -0800

    feat: Added network check

--- user_file/setup.sh
+++ user_file/setup.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 source utils.sh
 set -e
+function cleanup {
+    rm -f ~/first_login_setup_in_progress
+}
+trap cleanup EXIT
 #---------------------------------------------------------------------
 
 mkdir -p ~/first_login_log
+
+# source: 
+# https://unix.stackexchange.com/questions/190513/shell-scripting-proper-way-to-check-for-internet-connectivity
+if ! nc -zw1 google.com 443; then                                  
+print_error "\nNetwork not connected please enable network and then press any button to continue\n"
+empty_input_buffer
+read input
+fi
+
 print_message "Starting First Login Setup, Please Be Ready to Enter Sudo Password, this will take about 20 minutes\n"
 
 print_section "Configuring Miscellaneous Items"
