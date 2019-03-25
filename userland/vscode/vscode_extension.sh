4f53dc7e6836a7fb86e289dbf302b62135abf070
khoitd1997 <khoidinhtrinh@gmail.com>
2019-03-24 23:23:50 -0700

    refactor: Reorganized vscode extension page

--- userland/vscode/vscode_extension.sh
+++ userland/vscode/vscode_extension.sh
@@ -1,47 +1,52 @@
 #!/bin/bash
 
-extension_general="ms-vscode.cpptools  kevinkyang.auto-comment-blocks CoenraadS.bracket-pair-colorizer formulahendry.code-runner \
-		eamodio.gitlens donjayamanne.githistory huizhou.githd vscode-icons-team.vscode-icons webfreak.debug  \
-		wayou.vscode-todo-highlight emilast.logfilehighlighter Tyriar.sort-lines Gimly81.matlab \
-		PeterJausovec.vscode-docker oderwat.indent-rainbow rashwell.tcl \
-		vector-of-bool.cmake-tools twxs.cmake eugenwiens.bitbake redhat.vscode-yaml \
-		zhoufeng.pyqt-integration pnp.polacode Gruntfuggly.todo-tree \
-		ibm.output-colorizer compulim.vscode-clock github.vscode-pull-request-github mitaki28.vscode-clang 
-        ryuta46.multi-command vscodevim.vim \
-		laurenttreguier.rpm-spec bungcip.better-toml bbenoist.qml ionutvmi.path-autocomplete tickleforce.scrolloff hars.cppsnippets esbenp.prettier-vscode "
+extension_general=" kevinkyang.auto-comment-blocks CoenraadS.bracket-pair-colorizer formulahendry.code-runner webfreak.debug wayou.vscode-todo-highlight emilast.logfilehighlighter Tyriar.sort-lines oderwat.indent-rainbow rashwell.tcl eugenwiens.bitbake redhat.vscode-yaml Gruntfuggly.todo-tree ibm.output-colorizer compulim.vscode-clock ryuta46.multi-command vscodevim.vim laurenttreguier.rpm-spec bungcip.better-toml ionutvmi.path-autocomplete tickleforce.scrolloff esbenp.prettier-vscode "
 
-extension_theme=" zhuangtongfa.material-theme monokai.theme-monokai-pro-vscode "
+extension_theme=" monokai.theme-monokai-pro-vscode  vscode-icons-team.vscode-icons "
 
 extension_dropped=" vsciot-vscode.vscode-arduino"
 
-# specialized dev tools
+# programing languages
+extension_cpp=" ms-vscode.cpptools hars.cppsnippets vector-of-bool.cmake-tools twxs.cmake mitaki28.vscode-clang "
 extension_python=" ms-python.python njpwerner.autodocstring "
 extension_java=" redhat.java vscjava.vscode-java-debug naco-siren.gradle-language "
-extension_doxygen=" bbenoist.doxygen cschlosser.doxdocgen "
-extension_arm=" dan-c-underwood.arm marus25.cortex-debug "
 extension_vhdl=" puorc.awesome-vhdl "
 extension_verilog=" mshr-h.veriloghdl "
-extension_md=" mushan.vscode-paste-image DavidAnson.vscode-markdownlint yzhang.markdown-all-in-one \
-		shd101wyy.markdown-preview-enhanced yzane.markdown-pdf "
+extension_matlab=" Gimly81.matlab "
+extension_golang=" ms-vscode.go "
 
+# text marking languages
+extension_md=" mushan.vscode-paste-image DavidAnson.vscode-markdownlint yzhang.markdown-all-in-one shd101wyy.markdown-preview-enhanced yzane.markdown-pdf "
+extension_latex=" james-yu.latex-workshop "
+
+# tools
+extension_git=" eamodio.gitlens donjayamanne.githistory huizhou.githd github.vscode-pull-request-github "
+extension_doxygen=" bbenoist.doxygen cschlosser.doxdocgen "
+extension_arm=" dan-c-underwood.arm marus25.cortex-debug "
+extension_qt=" zhoufeng.pyqt-integration bbenoist.qml "
 extension_web=" formulahendry.auto-close-tag "
 extension_docker=" PeterJausovec.vscode-docker "
-extension_latex=" james-yu.latex-workshop "
-extension_golang=" ms-vscode.go "
 
 #-------------------------------------------------------------------------------------
 
 extension_all="${extension_general}${extension_theme}"
+
 extension_all="${extension_all}${extension_python}"
-extension_all="${extension_all}${extension_doxygen}"
-extension_all="${extension_all}${extension_arm}"
+extension_all="${extension_all}${extension_cpp}"
+extension_all="${extension_all}${extension_matlab}"
 # extension_all="${extension_all}${extension_vhdl}"
+extension_all="${extension_all}${extension_golang}"
+# extension_all="${extension_all}${extension_verilog}"
+extension_all="${extension_all}${extension_latex}"
 extension_all="${extension_all}${extension_java}"
 extension_all="${extension_all}${extension_md}"
+
+extension_all="${extension_all}${extension_git}"
+extension_all="${extension_all}${extension_doxygen}"
+extension_all="${extension_all}${extension_arm}"
+extension_all="${extension_all}${extension_qt}"
+extension_all="${extension_all}${extension_docker}"
 extension_all="${extension_all}${extension_web}"
-extension_all="${extension_all}${extension_latex}"
-extension_all="${extension_all}${extension_golang}"
-extension_all="${extension_all}${extension_verilog}"
 
 for ext in ${extension_all}
 do
