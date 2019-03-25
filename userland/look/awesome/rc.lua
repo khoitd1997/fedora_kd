75aae3d2e6431d14c9af99b4214f9ebd896f14d9
khoitd1997 <khoidinhtrinh@gmail.com>
2019-03-24 17:05:12 -0700

    feat: Added proper tiling policy

--- userland/look/awesome/rc.lua
+++ userland/look/awesome/rc.lua
@@ -276,102 +276,33 @@ awful.screen.connect_for_each_screen(
 
         -- Each screen has its own tag table.
         -- Tag layouts
-        local l = awful.layout.suit -- Alias to save time :)
-        -- local layouts = { l.max, l.floating, l.max, l.max , l.tile,
-        --     l.max, l.max, l.max, l.floating, l.tile}
+        local l = awful.layout.suit
         local layouts = {
-            l.tile,
-            l.tile,
-            l.tile,
-            l.tile,
-            l.tile,
-            l.tile,
-            l.tile,
-            l.tile,
-            l.tile,
-            l.tile
+            l.tile.right,
+            l.tile.right,
+            l.tile.right,
+            l.tile.right,
+            l.tile.right,
+            l.tile.right,
+            l.tile.right,
+            l.tile.right,
+            l.tile.right,
+            l.tile.right
         }
 
         -- Tag names
         local tagnames = beautiful.tagnames or {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
-
-        -- Create tags
-        awful.tag.add(
-            tagnames[1],
-            {
-                layout = layouts[1],
+        for i = 1, #tagnames do
+            local tagProp = {
+                layout = layouts[i],
                 screen = s,
-                selected = true
+                column_count = 3
             }
-        )
-        awful.tag.add(
-            tagnames[2],
-            {
-                layout = layouts[2],
-                screen = s
-            }
-        )
-        awful.tag.add(
-            tagnames[3],
-            {
-                layout = layouts[3],
-                screen = s
-            }
-        )
-        awful.tag.add(
-            tagnames[4],
-            {
-                layout = layouts[4],
-                master_width_factor = 0.6,
-                screen = s
-            }
-        )
-        awful.tag.add(
-            tagnames[5],
-            {
-                layout = layouts[5],
-                master_width_factor = 0.65,
-                screen = s
-            }
-        )
-        awful.tag.add(
-            tagnames[6],
-            {
-                layout = layouts[6],
-                screen = s
-            }
-        )
-        awful.tag.add(
-            tagnames[7],
-            {
-                layout = layouts[7],
-                screen = s
-            }
-        )
-        awful.tag.add(
-            tagnames[8],
-            {
-                layout = layouts[8],
-                screen = s
-            }
-        )
-        awful.tag.add(
-            tagnames[9],
-            {
-                layout = layouts[9],
-                screen = s
-            }
-        )
-        awful.tag.add(
-            tagnames[10],
-            {
-                layout = layouts[10],
-                screen = s
-            }
-        )
-
-        -- Create all tags at once (without seperate configuration for each tag)
-        -- awful.tag(tagnames, s, layouts)
+            if (i == 1) then
+                tagProp.selected = true
+            end
+            awful.tag.add(tagnames[i], tagProp)
+        end
     end
 )
 
