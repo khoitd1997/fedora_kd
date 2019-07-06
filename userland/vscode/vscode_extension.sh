6845bc15ccc61e7b862604a588def07f4a2828ed
khoitd1997 <khoidinhtrinh@gmail.com>
2019-07-06 10:37:34 -0700

    fix: Update vscode

--- userland/vscode/vscode_extension.sh
+++ userland/vscode/vscode_extension.sh
@@ -9,7 +9,7 @@ extension_dropped=" vsciot-vscode.vscode-arduino"
 # programing languages
 extension_cpp=" ms-vscode.cpptools hars.cppsnippets twxs.cmake "
 extension_csharp=" ms-vscode.csharp "
-extension_python=" ms-python.python njpwerner.autodocstring "
+extension_python=" ms-python.python njpwerner.autodocstring ms-pyright.pyright "
 extension_java=" redhat.java vscjava.vscode-java-debug naco-siren.gradle-language "
 extension_vhdl=" puorc.awesome-vhdl "
 extension_verilog=" mshr-h.veriloghdl "
