1d50222a0ee0a213b92f9945888b85d8a66072c2
khoitd1997 <khoidinhtrinh@gmail.com>
2020-04-14 17:34:43 -0700

    Removed auto turn off script

--- userland/misc/helpful_script/auto_turnoff.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-currenttime=`date +"%H%M%S"`
-
-echo "I am auto turn-off script"
-shutdown -P 00:10
-if [[ "$currenttime" > "001000" ]] && [[ "$currenttime" < "063000" ]]; then
-  future_shutdown_time=$(date -d "3 minutes" +'%H:%M')
-  shutdown -P ${future_shutdown_time}
-fi
