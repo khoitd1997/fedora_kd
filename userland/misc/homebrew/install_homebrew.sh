0d4c90c03f2bd4be512d50b13a866bef44f2175d
khoitd1997 <khoidinhtrinh@gmail.com>
2020-04-26 17:38:09 -0700

    added homebrew stuffs

---
+++ userland/misc/homebrew/install_homebrew.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -d "~/.linuxbrew/Homebrew" ]; then
+    # from https://docs.brew.sh/Homebrew-on-Linux
+    git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
+    mkdir ~/.linuxbrew/bin
+    ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
+    eval $(~/.linuxbrew/bin/brew shellenv)
+
+    # taken from https://docs.brew.sh/Homebrew-on-Linux
+    test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+    test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+    echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+fi
+
+source ~/.profile
+brew install hexyl
