a384e9ea12009ac2488237f2fae723fb15b9e241
khoitd1997 <khoidinhtrinh@gmail.com>
2020-04-26 22:40:10 -0700

    added option for continuing task

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ currDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${currDir}
 source ./utils.sh
 
-./setup_deps.sh
+if [ ! -z "$1" ]; then 
+    ansible-playbook ./userland/setup.yml --ask-become-pass --start-at-task="$1"
+else 
+    ./setup_deps.sh
 
-ansible-playbook ./userland/setup.yml --ask-become-pass 
+    ansible-playbook ./userland/setup.yml --ask-become-pass 
+fi
