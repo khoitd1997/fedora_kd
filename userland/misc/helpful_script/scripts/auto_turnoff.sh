7cf075b3cdf2d4a99c88223a82c6987762cddb9e
khoitd1997 <khoidinhtrinh@gmail.com>
2020-05-05 10:08:27 -0700

    changed shutdown time

--- userland/misc/helpful_script/scripts/auto_turnoff.sh
+++ userland/misc/helpful_script/scripts/auto_turnoff.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 currenttime=`date +"%H%M%S"`
 
-evening_curfew="22:00"
+evening_curfew="21:30"
 shutdown -P ${evening_curfew} > /dev/null 2>&1
-if [[ "$currenttime" > "001000" ]] && [[ "$currenttime" < "063000" ]]; then
+if [[ "$currenttime" > "213000" ]] && [[ "$currenttime" < "073000" ]]; then
   future_shutdown_time=$(date -d "3 minutes" +'%H:%M')
   shutdown -P ${future_shutdown_time} > /dev/null 2>&1
 fi
