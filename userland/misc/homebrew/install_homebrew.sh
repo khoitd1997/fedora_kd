8062d834df8759bf848f1f48b95f8269c3a1dd36
khoitd1997 <khoidinhtrinh@gmail.com>
2020-05-15 16:34:55 -0700

    added fail catch for homebrew install

--- userland/misc/homebrew/install_homebrew.sh
+++ userland/misc/homebrew/install_homebrew.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ ! -d "~/.linuxbrew/Homebrew" ]; then
     # from https://docs.brew.sh/Homebrew-on-Linux
     git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
