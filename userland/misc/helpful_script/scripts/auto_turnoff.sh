2af6633f099fc5d8336c927eccefd10c7f9f394d
khoitd1997 <khoidinhtrinh@gmail.com>
2020-07-06 17:11:15 -0700

    initial debian fix

--- userland/misc/helpful_script/scripts/auto_turnoff.sh
+++ userland/misc/helpful_script/scripts/auto_turnoff.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 currenttime=`date +"%H%M%S"`
 
-evening_curfew="21:30"
+evening_curfew="23:30"
 shutdown -P ${evening_curfew} > /dev/null 2>&1
-if [[ "$currenttime" > "213000" ]] && [[ "$currenttime" < "073000" ]]; then
+if [[ "$currenttime" > "233000" ]] && [[ "$currenttime" < "073000" ]]; then
   future_shutdown_time=$(date -d "3 minutes" +'%H:%M')
   shutdown -P ${future_shutdown_time} > /dev/null 2>&1
 fi
