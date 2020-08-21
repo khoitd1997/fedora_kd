66edbd6e7188eaddb044b211b6352f4d91be55cf
khoitd1997 <khoidinhtrinh@gmail.com>
2020-08-21 09:52:35 -0700

    added gnome stuffs

--- userland/look/gnome-terminal/configure_gnome_terminal.sh
+++ userland/look/gnome-terminal/configure_gnome_terminal.sh
@@ -5,6 +5,7 @@ currDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${currDir}
 #----------------------------------------------------------------------
 
+# dconf dump /org/gnome/terminal/ > /home/kd/fedora_kd/userland/look/gnome-terminal/gnome-terminal-backup.txt
 gnome-terminal&
 sleep 3
 dconf load /org/gnome/terminal/ < ./gnome-terminal-backup.txt
