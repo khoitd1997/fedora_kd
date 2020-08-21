8b0f10dc0c08278bc881b84f74d905ffdda9f496
khoitd1997 <khoidinhtrinh@gmail.com>
2020-08-21 09:35:41 -0700

    clean up vscode extensions

--- userland/vscode/vscode_extension.sh
+++ userland/vscode/vscode_extension.sh
@@ -1,13 +1,43 @@
 #!/bin/bash
 
-extension_general=" coenraads.bracket-pair-colorizer-2 webfreak.debug emilast.logfilehighlighter oderwat.indent-rainbow rashwell.tcl eugenwiens.bitbake redhat.vscode-yaml Gruntfuggly.todo-tree ibm.output-colorizer compulim.vscode-clock ryuta46.multi-command vscodevim.vim laurenttreguier.rpm-spec bungcip.better-toml ionutvmi.path-autocomplete wk-j.save-and-run dakara.dakara-foldplus alefragnani.bookmarks ms-vsliveshare.vsliveshare-pack ms-vscode-remote.vscode-remote-extensionpack visualstudioexptteam.vscodeintellicode jack89ita.copy-filename ryu1kn.partial-diff grapecity.gc-excelviewer aaron-bond.better-comments gaborv.flatbuffers bierner.color-info kisstkondoros.vscode-gutter-preview sleistner.vscode-fileutils marcostazi.vs-code-vagrantfile k--kato.intellij-idea-keybindings streetsidesoftware.code-spell-checker esbenp.prettier-vscode jebbs.plantuml platformio.platformio-ide ml.nc-gcode "
+extension_general=" coenraads.bracket-pair-colorizer-2 \
+                    emilast.logfilehighlighter \
+                    oderwat.indent-rainbow \
+                    rashwell.tcl \
+                    eugenwiens.bitbake \
+                    redhat.vscode-yaml \
+                    Gruntfuggly.todo-tree \
+                    ibm.output-colorizer \
+                    compulim.vscode-clock \
+                    ryuta46.multi-command \
+                    vscodevim.vim \
+                    bungcip.better-toml \
+                    ionutvmi.path-autocomplete \
+                    wk-j.save-and-run \
+                    dakara.dakara-foldplus \
+                    alefragnani.bookmarks \
+                    ms-vscode-remote.vscode-remote-extensionpack \
+                    visualstudioexptteam.vscodeintellicode \
+                    jack89ita.copy-filename \
+                    ryu1kn.partial-diff \
+                    grapecity.gc-excelviewer \
+                    aaron-bond.better-comments \
+                    gaborv.flatbuffers \
+                    sleistner.vscode-fileutils \
+                    marcostazi.vs-code-vagrantfile \
+                    k--kato.intellij-idea-keybindings \
+                    esbenp.prettier-vscode \
+                    jebbs.plantuml "
 
-extension_theme=" monokai.theme-monokai-pro-vscode vscode-icons-team.vscode-icons emmanuelbeziat.vscode-great-icons zhuangtongfa.material-theme "
-
-extension_dropped=" vsciot-vscode.vscode-arduino"
+extension_theme=" zhuangtongfa.material-theme "
 
 # programing languages
-extension_cpp=" ms-vscode.cpptools hars.cppsnippets twxs.cmake asabil.meson ms-vscode.cmake-tools "
+extension_cpp=" ms-vscode.cpptools \
+                hars.cppsnippets \
+                twxs.cmake \
+                asabil.meson \
+                ms-vscode.cmake-tools "
+
 extension_csharp=" ms-dotnettools.csharp "
 extension_python=" ms-python.python njpwerner.autodocstring "
 extension_java=" redhat.java vscjava.vscode-java-debug naco-siren.gradle-language "
@@ -15,18 +45,35 @@ extension_vhdl=" puorc.awesome-vhdl "
 extension_verilog=" mshr-h.veriloghdl "
 extension_matlab=" Gimly81.matlab "
 extension_golang=" ms-vscode.go "
+extension_gcode=" ml.nc-gcode "
 
 # text marking languages
-extension_md=" mushan.vscode-paste-image DavidAnson.vscode-markdownlint yzhang.markdown-all-in-one shd101wyy.markdown-preview-enhanced yzane.markdown-pdf "
+extension_md=" mushan.vscode-paste-image \
+               DavidAnson.vscode-markdownlint \
+               yzhang.markdown-all-in-one \
+               shd101wyy.markdown-preview-enhanced \
+               yzane.markdown-pdf "
+
 extension_latex=" james-yu.latex-workshop "
 
 # tools
-extension_git=" eamodio.gitlens donjayamanne.githistory github.vscode-pull-request-github mhutchie.git-graph "
+extension_git=" eamodio.gitlens \
+                donjayamanne.githistory \
+                github.vscode-pull-request-github \
+                mhutchie.git-graph "
+
 extension_doxygen=" cschlosser.doxdocgen "
 extension_arm=" dan-c-underwood.arm marus25.cortex-debug "
 extension_qt=" zhoufeng.pyqt-integration bbenoist.qml "
-extension_web=" formulahendry.auto-close-tag formulahendry.auto-rename-tag vincaslt.highlight-matching-tag "
+
+extension_web=" formulahendry.auto-close-tag \
+                formulahendry.auto-rename-tag \
+                vincaslt.highlight-matching-tag \
+                kisstkondoros.vscode-gutter-preview "
+
 extension_docker=" ms-azuretools.vscode-docker "
+extension_liveshare=" ms-vsliveshare.vsliveshare-pack "
+extension_embedded=" platformio.platformio-ide webfreak.debug "
 
 #-------------------------------------------------------------------------------------
 
@@ -42,12 +89,15 @@ extension_all="${extension_all}${extension_matlab}"
 extension_all="${extension_all}${extension_latex}"
 extension_all="${extension_all}${extension_java}"
 extension_all="${extension_all}${extension_md}"
+extension_all="${extension_all}${extension_gcode}"
+extension_all="${extension_all}${extension_embedded}"
 
 extension_all="${extension_all}${extension_git}"
 extension_all="${extension_all}${extension_doxygen}"
 extension_all="${extension_all}${extension_arm}"
 extension_all="${extension_all}${extension_qt}"
 extension_all="${extension_all}${extension_docker}"
+extension_all="${extension_all}${extension_liveshare}"
 extension_all="${extension_all}${extension_web}"
 
 for ext in ${extension_all}
