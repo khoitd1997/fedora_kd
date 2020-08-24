ae3e065714901ceae59f3422bfeadf6db017754f
khoitd1997 <khoidinhtrinh@gmail.com>
2020-08-23 22:19:48 -0700

    alt+jk for fish

--- userland/fish/data/config.fish
+++ userland/fish/data/config.fish
@@ -2,6 +2,12 @@ source /usr/share/doc/fzf/examples/key-bindings.fish
 fzf_key_bindings
 
 bind \cd accept-autosuggestion
+bind \ej down-or-search
+bind \ek history-token-search-backward
+bind \ek up-or-search
+bind \ej history-token-search-forward
+
+export FZF_DEFAULT_OPTS='--bind alt-j:down,alt-k:up'
 
 alias ls='exa'
 alias bat='batcat'
