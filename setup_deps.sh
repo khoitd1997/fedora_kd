45c708abe48144d9eba2f5186758eb9b4f93404a
khoitd1997 <khoidinhtrinh@gmail.com>
2020-10-10 11:25:33 -0700

    fixed small bugs

--- setup_deps.sh
+++ setup_deps.sh
@@ -4,13 +4,14 @@ distro=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
 
 if [ "$distro" = "Ubuntu" ]; then
     sudo apt update
-    sudo apt install python3-pip python-psutil build-essential zsh ansible perl -y
+    sudo apt install python3-pip curl python-psutil build-essential zsh ansible perl -y
     sudo -E pip3 install fexpect -q
 else
     sudo dnf install -q -y \
         @development-tools \
         perl-core \
         ansible \
+        curl \
         zsh \
         zsh-syntax-highlighting \
         dnf-plugins-core
