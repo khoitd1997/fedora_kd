ba9bb61af5465a28b83c1ae1b0b506c5bb685d9a
khoitd1997 <khoidinhtrinh@gmail.com>
2021-04-17 00:23:00 -0700

    fixed auto turn off script

--- userland/misc/helpful_script/scripts/auto_turnoff.sh
+++ userland/misc/helpful_script/scripts/auto_turnoff.sh
@@ -4,6 +4,5 @@ currenttime=`date +"%H%M"`
 evening_curfew="22:30"
 shutdown -P ${evening_curfew} > /dev/null 2>&1
 if [[ "$currenttime" > "2230" ]] || [[ "$currenttime" < "0730" ]]; then
-  future_shutdown_time=$(date -d "2 minutes" +'%H:%M')
-  shutdown -P ${future_shutdown_time} > /dev/null 2>&1
+  shutdown -h now > /dev/null 2>&1
 fi
