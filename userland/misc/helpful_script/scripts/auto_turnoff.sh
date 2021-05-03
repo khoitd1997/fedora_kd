e22ca652f3e21bd550d8e2c72773022c5bffc42f
khoitd1997 <khoidinhtrinh@gmail.com>
2021-05-02 20:47:16 -0700

    Changed curfew time to 2200

--- userland/misc/helpful_script/scripts/auto_turnoff.sh
+++ userland/misc/helpful_script/scripts/auto_turnoff.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 currenttime=`date +"%H%M"`
 
-evening_curfew="22:30"
+evening_curfew="22:00"
 shutdown -P ${evening_curfew} > /dev/null 2>&1
-if [[ "$currenttime" > "2230" ]] || [[ "$currenttime" < "0730" ]]; then
+if [[ "$currenttime" > "2200" ]] || [[ "$currenttime" < "0730" ]]; then
   shutdown -h now > /dev/null 2>&1
 fi
