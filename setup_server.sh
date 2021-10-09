667f5ad74a750961a2e9bd90f954db0717345541
khoitd1997 <khoidinhtrinh@gmail.com>
2021-10-09 11:31:25 -0700

    added server config

---
+++ setup_server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd ${script_dir}
+
+ansible-playbook ./userland/setup.yml --ask-become-pass --tags server
