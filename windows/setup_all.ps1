b4c07cfd05050c32b155591261fc4d861de9a58e
Khoi Dinh Trinh <khoidinhtrinh@gmail.com>
2021-10-13 01:22:34 -0700

    some fix up

--- windows/setup_all.ps1
+++ windows/setup_all.ps1
@@ -5,15 +5,15 @@
 LogHeader "Starting Windows Configuration"
 
 try {
-    # & "$PSScriptRoot\sw_install\install_software.ps1"
+    & "$PSScriptRoot\sw_install\install_software.ps1"
 
-    # & "$PSScriptRoot\autohotkey\setup_autohotkey.ps1"
+    & "$PSScriptRoot\autohotkey\setup_autohotkey.ps1"
 
-    # & "$PSScriptRoot\vscode\setup_vscode.ps1"
+    & "$PSScriptRoot\vscode\setup_vscode.ps1"
 
-    # & "$PSScriptRoot\windows_terminal\setup_windows_terminal.ps1"
+    & "$PSScriptRoot\windows_terminal\setup_windows_terminal.ps1"
 
-    # & "$PSScriptRoot\wsl\wsl_setup.ps1"
+    & "$PSScriptRoot\wsl\wsl_setup.ps1"
 }
 catch {
     LogError "$PSItem"
