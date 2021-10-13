0c78ecffa52803658a6d9a76cdc645927353a664
Khoi Dinh Trinh <khoidinhtrinh@gmail.com>
2021-10-13 01:03:09 -0700

    added some server stuff

--- windows/setup_all.ps1
+++ windows/setup_all.ps1
@@ -2,20 +2,18 @@
 
 . $PSScriptRoot\utils.ps1
 
-
 LogHeader "Starting Windows Configuration"
 
 try {
-    & "$PSScriptRoot\sw_install\install_software.ps1"
+    # & "$PSScriptRoot\sw_install\install_software.ps1"
 
-    & "$PSScriptRoot\autohotkey\setup_autohotkey.ps1"
+    # & "$PSScriptRoot\autohotkey\setup_autohotkey.ps1"
 
-    & "$PSScriptRoot\vscode\setup_vscode.ps1"
+    # & "$PSScriptRoot\vscode\setup_vscode.ps1"
 
-    & "$PSScriptRoot\windows_terminal\setup_windows_terminal.ps1"
+    # & "$PSScriptRoot\windows_terminal\setup_windows_terminal.ps1"
 
-    & "$PSScriptRoot\wsl\wsl_setup.ps1"
-
+    # & "$PSScriptRoot\wsl\wsl_setup.ps1"
 }
 catch {
     LogError "$PSItem"
@@ -23,4 +21,4 @@ catch {
 
 LogHeader "Finished Windows Configurations"
 
-Start-Sleep -Seconds 1000
+Start-Sleep -Seconds 60
