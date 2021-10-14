6b58cb0f9bbfb9c0b77fc3adbfe580e62fe7c3e0
khoitd1997 <khoidinhtrinh@gmail.com>
2021-10-13 21:48:24 -0700

    updated some stuff

--- linux/setup_server.sh
+++ linux/setup_server.sh
@@ -5,4 +5,7 @@ set -e
 script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${script_dir}
 
-ansible-playbook ./userland/setup.yml --ask-become-pass --tags server
+ansible-playbook ./userland/setup.yml \
+    --ask-become-pass \
+    --tags server \
+    --extra-vars "variable_host=kd-server"
