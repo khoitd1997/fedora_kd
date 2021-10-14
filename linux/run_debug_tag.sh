a3ae3e15de2616e1c97f47fa1e2fb6fa2c07c826
khoitd1997 <khoidinhtrinh@gmail.com>
2021-10-13 23:59:10 -0700

    Made nfs work better

--- linux/run_debug_tag.sh
+++ linux/run_debug_tag.sh
@@ -5,4 +5,10 @@ set -e
 script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${script_dir}
 
-ansible-playbook ./userland/setup.yml --ask-become-pass --tags="debug"
+# extra_var="variable_host=kd-server"
+
+ansible-playbook ./userland/setup.yml \
+    --ask-become-pass \
+    -i ${script_dir}/inventory \
+    --tags="debug" \
+    --extra-vars "${extra_var}"
