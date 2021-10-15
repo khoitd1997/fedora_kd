0f6b65baefb4b6ce444eeca7c65e78e1da931814
Khoi Trinh <khoidinhtrinh@gmail.com>
2021-10-15 14:24:53 -0700

    added some convenient commands

--- windows/windows_terminal/powershell_profile.ps1
+++ windows/windows_terminal/powershell_profile.ps1
@@ -4,6 +4,9 @@ CUSTOM COMMAND LIST:
 ssh-to-server: ssh to home server
 mount-bulk-share: mount the bulk share to Z: drive
 
+sha256sum <file_path>: take sha256 hash of a file
+extract-tar <file-to-extract> [destination]: extract a .tar.gz file
+
 "@ -ForegroundColor black -BackgroundColor white
 
 function ssh-to-server {
@@ -14,6 +17,21 @@ function mount-bulk-share {
     Write-Host "Powershell terminal seems to have problems when entering credentials, use cmd.exe if there is an issue"
     Write-Host "If asked for password, use Linux credentials (ie the kd account)"
     net use z: \\kd-server\Bulk_Storage_Share /savecred /persistent:yes
+}
+
+function sha256sum {
+    param (
+        [string]$FilePath
+    )
+    Get-FileHash $FilePath -Algorithm SHA256 | Format-List
+}
+
+function extract-tar {
+    param (
+        [string]$FilePath,
+        [string]$DestPath = "."
+    )
+    tar -xvzf $FilePath -C $DestPath
 }
 
 if ($host.Name -eq 'ConsoleHost') {
