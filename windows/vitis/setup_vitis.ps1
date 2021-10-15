8027614e030aad68c196e30d3a925e1e884afaf6
khoitd1997 <khoidinhtrinh@gmail.com>
2021-10-15 10:48:35 -0700

    added install command

--- windows/vitis/setup_vitis.ps1
+++ windows/vitis/setup_vitis.ps1
@@ -2,3 +2,5 @@ Set-Location "G:\Downloads\Xilinx_Unified_2020.1_0602_1208"
 
 tar -xvzf ".\Xilinx_Unified_2020.1_0602_1208.tar.gz"
 tar -xvzf ".\Xilinx_Vivado_Vitis_Update_2020.1.1_0805_2247.tar.gz"
+
+./xsetup.exe -c ./install_config.txt --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Add
