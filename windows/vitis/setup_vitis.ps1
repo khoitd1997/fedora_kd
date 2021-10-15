ccf6f103cb9342c65c237fe57356216c81108716
Khoi Trinh <khoidinhtrinh@gmail.com>
2021-10-15 10:06:58 -0700

    added setup_vitis.ps1

---
+++ windows/vitis/setup_vitis.ps1
@@ -0,0 +1,4 @@
+Set-Location "G:\Downloads\Xilinx_Unified_2020.1_0602_1208"
+
+tar -xvzf ".\Xilinx_Unified_2020.1_0602_1208.tar.gz"
+tar -xvzf ".\Xilinx_Vivado_Vitis_Update_2020.1.1_0805_2247.tar.gz"
