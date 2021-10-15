2caf6ebd06ea009a5bedcaf5562f3832034460cd
Khoi Trinh <khoidinhtrinh@gmail.com>
2021-10-15 14:37:41 -0700

    added vitis setup

--- windows/vitis/setup_vitis.ps1
+++ windows/vitis/setup_vitis.ps1
@@ -1,6 +1,8 @@
-Set-Location "G:\Downloads\Xilinx_Unified_2020.1_0602_1208"
+Set-Location "G:\Downloads\Xilinx_Unified_2021.1_0610_2318"
 
-tar -xvzf ".\Xilinx_Unified_2020.1_0602_1208.tar.gz"
-tar -xvzf ".\Xilinx_Vivado_Vitis_Update_2020.1.1_0805_2247.tar.gz"
+# tar -xvzf ".\Xilinx_Unified_2020.1_0602_1208.tar.gz"
+# tar -xvzf ".\Xilinx_Vivado_Vitis_Update_2020.1.1_0805_2247.tar.gz"
 
-./xsetup.exe -c ./install_config.txt --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Add
+./xsetup.exe -c "$PSScriptRoot/install_config.txt" --agree "XilinxEULA,3rdPartyEULA,WebTalkTerms" --batch Add
+
+Set-Location "$PSScriptRoot"
