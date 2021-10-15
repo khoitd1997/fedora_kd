c4f40777c3f47fef4607d76b7ba637843b91e978
Khoi Trinh <khoidinhtrinh@gmail.com>
2021-10-15 12:04:52 -0700

    Added setting up pwsh 7

--- windows/windows_terminal/setup_windows_terminal.ps1
+++ windows/windows_terminal/setup_windows_terminal.ps1
@@ -5,6 +5,9 @@ New-Item -Path "$profile" `
     -Value "$PSScriptRoot\powershell_profile.ps1"`
     -Force
 
+# set up profile for powershell 7
+pwsh -c { New-Item -Path "$profile" -ItemType SymbolicLink -Value "$((Get-Item -Path .\ -Verbose).FullName)\powershell_profile.ps1" -Force } -WorkingDirectory $PSScriptRoot
+
 New-Item -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" `
     -ItemType SymbolicLink `
     -Value "$PSScriptRoot\terminal_settings.json"`
