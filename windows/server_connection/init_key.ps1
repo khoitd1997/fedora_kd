9c1254cdb322442c86f3ea959f01c88d5c86092e
Khoi Trinh <khoidinhtrinh@gmail.com>
2021-10-15 21:41:47 -0700

    Added some commands for ps

--- windows/server_connection/init_key.ps1
+++
@@ -1,8 +0,0 @@
-# used for first time set up of ssh key on the server
-
-ssh-keygen.exe
-
-type $env:USERPROFILE\.ssh\id_rsa.pub | ssh kd@"kd-server" "cat >> .ssh/authorized_keys"
-
-$From = Get-Content -Path $PSScriptRoot\ssh_config
-Add-Content -Path $env:USERPROFILE\.ssh\config -Value $From
--- windows/windows_terminal/powershell_profile.ps1
+++ windows/windows_terminal/powershell_profile.ps1
@@ -1,6 +1,9 @@
 Write-Host @"
 CUSTOM COMMAND LIST:
 
+vscode-ssh <path-on-server> [server-host-name]: open a remote vscode session
+
+init-server-key: for first time initialization of ssh key
 ssh-to-server: ssh to home server
 mount-bulk-share: mount the bulk share to Z: drive
 
@@ -8,6 +11,12 @@ sha256sum <file_path>: take sha256 hash of a file
 extract-tar <file-to-extract> [destination]: extract a .tar.gz file
 
 "@ -ForegroundColor black -BackgroundColor white
+
+function init-server-key {
+    # ssh-keygen.exe
+
+    Get-Content $env:USERPROFILE\.ssh\id_rsa.pub | ssh kd@"kd-server" "cat >> .ssh/authorized_keys"
+}
 
 function ssh-to-server {
     ssh kd@kd-server
@@ -17,6 +26,18 @@ function mount-bulk-share {
     Write-Host "Powershell terminal seems to have problems when entering credentials, use cmd.exe if there is an issue"
     Write-Host "If asked for password, use Linux credentials (ie the kd account)"
     net use z: \\kd-server\Bulk_Storage_Share /savecred /persistent:yes
+}
+
+function vscode-ssh {
+    param(
+        [string]$PathOnServer,
+        [string]$ServerHostName = "kd-server"
+    )
+
+    Write-Output "Host: ${ServerHostName}"
+    Write-Output "Path: ${PathOnServer}"
+
+    code --folder-uri "vscode-remote://ssh-remote+${ServerHostName}/${PathOnServer}"
 }
 
 function sha256sum {
