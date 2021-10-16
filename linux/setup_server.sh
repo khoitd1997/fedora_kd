6a59faced0b91fb80c1408f03dd1bfec050084f1
khoitd1997 <khoidinhtrinh@gmail.com>
2021-10-15 18:34:46 -0700

    Fixed some stuff

--- linux/setup_server.sh
+++ linux/setup_server.sh
@@ -5,8 +5,15 @@ set -e
 script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${script_dir}
 
+# if this is being run locally on the server
+# then use localhost otherwise use the server hostname
+extra_var="variable_host=kd-server"
+if [ $(hostname) = "kd-server" ]; then
+    extra_var=""
+fi
+
 ansible-playbook ./userland/setup.yml \
     --ask-become-pass \
     --tags server \
     -i ${script_dir}/inventory \
-    --extra-vars "variable_host=kd-server"
+    --extra-vars "${extra_var}"
