30aafb4b4ca380bd0cc5bfa4781c60806315fc89
khoitd1997 <khoidinhtrinh@gmail.com>
2021-10-23 16:15:30 -0700

    Added some pam gnome stuff

--- linux/run_debug_tag.sh
+++ linux/run_debug_tag.sh
@@ -5,7 +5,8 @@ set -e
 script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${script_dir}
 
-# extra_var="variable_host=kd-server"
+extra_var="variable_host=client_group"
+extra_var="variable_host=server_group"
 
 ansible-playbook ./userland/setup.yml \
     --ask-become-pass \
