7768c18aae6cca1e0a77169ea4083b6cdadb27e9
Khoi Trinh <khoidinhtrinh@gmail.com>
2021-10-24 14:40:14 -0700

    added terminal admin shortcut

--- windows/misc_setup.ps1
+++ windows/misc_setup.ps1
@@ -1,2 +1,13 @@
-AddToEnvironmentVariable "Path" "G:\Xilinx\Vitis\2020.1\bin"
-AddToEnvironmentVariable "Path" "G:\Xilinx\Vivado\2020.1\bin"
+function CreateAdminTerminalShortcut() {
+    $DesktopFolderPath = [Environment]::GetFolderPath("Desktop")
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("$DesktopFolderPath\Windows Terminal Admin.lnk")
+    $Shortcut.TargetPath = "pwsh.exe"
+    $Shortcut.Arguments = '-Command Start-Process -FilePath "wt" -Verb RunAs'
+    $Shortcut.Save()
+}
+
+AddToEnvironmentVariable "Path" "C:\Xilinx\Vitis\2020.1\bin"
+AddToEnvironmentVariable "Path" "C:\Xilinx\Vivado\2020.1\bin"
+
+CreateAdminTerminalShortcut
