c720f36d2933a7f17104736bfd1163d071365f20
Khoi Trinh <khoidinhtrinh@gmail.com>
2021-10-28 08:45:57 -0700

    Added more powertoys settings

--- windows/powertoys/setup_powertoys.ps1
+++ windows/powertoys/setup_powertoys.ps1
@@ -1,5 +1,9 @@
 $PowerToysAppDataDir = "$env:LOCALAPPDATA\Microsoft\PowerToys"
 
+$null = New-Item -Path "$PowerToysAppDataDir\settings.json" `
+    -ItemType SymbolicLink `
+    -Value "$PSScriptRoot/settings.json"`
+    -Force
 $null = New-Item -Path "$PowerToysAppDataDir\File Explorer\settings.json" `
     -ItemType SymbolicLink `
     -Value "$PSScriptRoot/file_explorer/settings.json"`
