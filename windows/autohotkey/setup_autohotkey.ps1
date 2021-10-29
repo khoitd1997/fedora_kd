82cf3667d8df1102b106e8378757e611183f4b8f
Khoi Trinh <khoidinhtrinh@gmail.com>
2021-10-29 09:49:47 -0700

    reorganized the autohotkey folder

--- windows/autohotkey/setup_autohotkey.ps1
+++ windows/autohotkey/setup_autohotkey.ps1
@@ -1,4 +1,21 @@
-$null = New-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\autohotkey_script.ahk" `
-    -ItemType SymbolicLink `
-    -Value "$PSScriptRoot\autohotkey_script.ahk" `
-    -Force
+. $PSScriptRoot\..\utils.ps1
+
+LogHeader "Setting Up Autohotkey"
+
+$StartupFolder = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
+
+LogHeader "Removing Current ahk Script in the startup folder"
+Get-ChildItem "$StartupFolder" -Filter *.ahk | 
+Foreach-Object {
+    Remove-Item "$($_.FullName)"
+}
+
+Get-ChildItem "$PSScriptRoot\scripts_loaded_at_startup" -Filter *.ahk | 
+Foreach-Object {
+    $null = New-Item -Path "${StartupFolder}\$($_.Name)" `
+        -ItemType SymbolicLink `
+        -Value "$($_.FullName)" `
+        -Force
+}
+
+LogHeader "Finished Setting Up Autohotkey"
