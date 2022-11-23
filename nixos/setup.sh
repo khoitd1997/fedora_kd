867b886f874abb2b7d94c490307b54e84ec3dfaf
khoitd1997 <khoidinhtrinh@gmail.com>
2022-11-22 23:06:15 -0800

    fixed gnome stuffs

--- nixos/setup.sh
+++ nixos/setup.sh
@@ -3,13 +3,20 @@
 script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd "${script_dir}"
 
+if [ "$EUID" -eq 0 ]; then 
+    echo "This script cannot be run as root!"
+    exit 1
+fi
+
 sudo ln -sfv "${script_dir}/configuration.nix" /etc/nixos/configuration.nix
 
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable unstable
 sudo nix-channel --update
 
-sudo nixos-rebuild boot --upgrade
+sudo nixos-rebuild switch --upgrade
+
+dconf load / < "${script_dir}/gnome_settings.txt"
 
 bash "${script_dir}/../linux/userland/vscode/vscode_extension.sh"
 bash "${script_dir}/../linux/userland/vscode/vscode_configure.sh"
