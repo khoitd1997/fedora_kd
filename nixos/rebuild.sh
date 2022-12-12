f59f5e8021251b251b0f7ec67476b6de3634532c
khoitd1997 <khoidinhtrinh@gmail.com>
2022-12-11 21:29:10 -0800

    adjusted Nix rebuild

--- nixos/rebuild.sh
+++ nixos/rebuild.sh
@@ -3,4 +3,4 @@
 script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd "${script_dir}"
 
-sudo nixos-rebuild boot --flake '.#nixos-kd'
+sudo nixos-rebuild boot --install-bootloader --flake '.#nixos-kd'
