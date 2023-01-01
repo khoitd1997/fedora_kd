629f9f3d105946e3c6e924193c27e90721a03d7e
khoitd1997 <khoidinhtrinh@gmail.com>
2023-01-01 15:23:09 -0800

    bump dep

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -8,7 +8,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
+      # url = "github:nix-community/home-manager/release-22.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
