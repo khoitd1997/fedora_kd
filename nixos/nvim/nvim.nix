6245631fbb802f6913ff5eb6c670f2c40f354ed1
khoitd1997 <khoidinhtrinh@gmail.com>
2024-01-19 21:30:40 -0800

    Adjusted nvim

--- nixos/nvim/nvim.nix
+++ nixos/nvim/nvim.nix
@@ -74,6 +74,18 @@
 
         "ctrl+shift+n to search for files
         nmap <unique> <C-N> :Files<CR>
+
+        # use shift+K to show type
+        nnoremap <silent> K :call <SID>show_documentation()<CR>
+        function! s:show_documentation()
+          if (index(['vim','help'], &filetype) >= 0)
+            execute 'h '.expand('<cword>')
+          elseif (coc#rpc#ready())
+            call CocActionAsync('doHover')
+          else
+            execute '!' . &keywordprg . " " . expand('<cword>')
+          endif
+        endfunction
       '';
 
       plugins = with pkgs.vimPlugins; [
