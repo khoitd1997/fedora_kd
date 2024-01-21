a42ff3495e695a5c03cbffa727ce42ecc134706d
khoitd1997 <khoidinhtrinh@gmail.com>
2024-01-21 14:13:00 -0800

    change oil keybinding

--- nixos/nvim/nvim.nix
+++ nixos/nvim/nvim.nix
@@ -156,6 +156,22 @@
                 "mtime",
               },
 
+              use_default_keymaps = false,
+              keymaps = {
+                ["g?"] = "actions.show_help",
+                ["<CR>"] = "actions.select",
+                ["<C-s>"] = "actions.select_vsplit",
+                ["<C-t>"] = "actions.select_tab",
+                ["<C-p>"] = "actions.preview",
+                ["<C-l>"] = "actions.refresh",
+                ["-"] = "actions.parent",
+                ["_"] = "actions.open_cwd",
+                ["`"] = "actions.cd",
+                ["~"] = "actions.tcd",
+                ["gs"] = "actions.change_sort",
+                ["g."] = "actions.toggle_hidden",
+              },
+
               view_options = {
                 -- show all hidden files except for .git
                 is_hidden_file = function(name, bufnr)
