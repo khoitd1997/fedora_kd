309b0c8a8db0e56a0e019e3f6c73855a2efb61bb
khoitd1997 <khoidinhtrinh@gmail.com>
2024-01-21 12:43:08 -0800

    Added vim stuffs

--- nixos/nvim/nvim.nix
+++ nixos/nvim/nvim.nix
@@ -57,6 +57,16 @@
         statix
         nerdtree
         diffview-nvim
+        vim-fugitive
+
+        {
+          plugin = which-key-nvim;
+          type = "lua";
+          config = ''
+            local wk = require("which-key")
+            wk.register({})
+          '';
+        }
 
         {
           plugin = lualine-nvim;
@@ -297,29 +307,33 @@
 
                 -- Buffer local mappings.
                 -- See `:help vim.lsp.*` for documentation on any of the below functions
-                local opts = { buffer = ev.buf }
+                mkOpts = function(desc)
+                  local opts = { buffer = ev.buf, desc = desc }
+                  return opts
+                end
 
                 -- gd goes to defintion
                 -- K to show hover
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, mkOpts("Go to declaration"))
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, mkOpts("Go to definition"))
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, mkOpts("Trigger hover"))
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, mkOpts("Go to implementation"))
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, mkOpts("Show references"))
 
-                vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-                vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, mkOpts(""))
+
+                vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, mkOpts(""))
+                vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, mkOpts(""))
                 vim.keymap.set('n', '<space>wl', function()
                   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                end, opts)
-                vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-                vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                  end, mkOpts(""))
+                vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, mkOpts("Show type definition"))
+                vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, mkOpts("Rename"))
 
-                vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, mkOpts("Code action"))
                 vim.keymap.set('n', '<space>f', function()
-                vim.lsp.buf.format { async = true }
-                end, opts)
+                  vim.lsp.buf.format { async = true }
+                  end, mkOpts(""))
               end,
             })
           '';
