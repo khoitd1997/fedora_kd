4ec626c1fa7f507370abf47defb42d138f8c0d4a
khoitd1997 <khoidinhtrinh@gmail.com>
2024-01-25 22:27:44 -0800

    bump

--- nixos/update.sh
+++ nixos/update.sh
@@ -5,7 +5,7 @@ set -e
 script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd "${script_dir}"
 
-release_version="22.11"
+release_version="23.11"
 
 sudo nix-channel --add https://nixos.org/channels/"nixos-${release_version}" nixos
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-${release_version}.tar.gz home-manager
