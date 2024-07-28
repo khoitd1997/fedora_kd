e2f9ba01affc0b05bd34e09b895c02cfc748d54b
khoitd1997 <khoidinhtrinh@gmail.com>
2024-07-28 14:58:19 -0700

    kicad theme

--- nixos/kicad/kicad.nix
+++ nixos/kicad/kicad.nix
@@ -1,7 +1,20 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 let
   myKicad = pkgs.kicad.override { };
-  kicadPluginsDir = ".local/share/kicad/${lib.versions.majorMinor myKicad.version}/scripting";
+  kicadVersion = lib.versions.majorMinor myKicad.version;
+  kicadPluginsDir = ".local/share/kicad/${kicadVersion}/scripting";
+  kicadThemeDir = ".local/share/kicad/${kicadVersion}/3rdparty/colors";
+
+  kicadTheme = pkgs.fetchFromGitHub {
+    owner = "pointhi";
+    repo = "kicad-color-schemes";
+    rev = "68ea0402f334bdbae175f6ca924743640c07183d";
+    hash = "sha256-PYgFOyK5MyDE1vTkz5jGnPWAz0pwo6Khu91ANgJ2OO4=";
+  };
+
+  # NOTE: For a list of possible themes, check the folder names in https://github.com/pointhi/kicad-color-schemes/tree/master
+  kicadSchematicTheme = "wdark";
+  kicadPcbNewTheme = "behave-dark";
 in
 {
   home.file."${kicadPluginsDir}/InteractiveHtmlBom".source = pkgs.fetchFromGitHub
@@ -18,6 +31,23 @@ in
     rev = "8c2cb8abd5559a79b0961212010d83d42bdeeca7";
     hash = "sha256-PHBrSxjcUMXPiFQSABA1tR/deinTwl3KdsLCfDR9F1o=";
   };
+
+  home.file."${kicadThemeDir}/${kicadSchematicTheme}/${kicadSchematicTheme}.json".source = kicadTheme + "/${kicadSchematicTheme}/${kicadSchematicTheme}.json";
+  home.file."${kicadThemeDir}/${kicadPcbNewTheme}/${kicadPcbNewTheme}.json".source = kicadTheme + "/${kicadPcbNewTheme}/${kicadPcbNewTheme}.json";
+
+  home.activation.kicadConfig = lib.getExe (pkgs.writeShellApplication {
+    name = "kicad-config";
+    runtimeInputs = with pkgs; [ jq moreutils ];
+    text = ''
+      # schematic theme
+      eeschema_conf="${config.xdg.configHome}/kicad/${kicadVersion}/eeschema.json"
+      jq '.appearance.color_theme = "${config.home.homeDirectory}/${kicadThemeDir}/${kicadSchematicTheme}/${kicadSchematicTheme}.json"' $eeschema_conf | sponge $eeschema_conf
+
+      # pcb layout theme
+      pcbnew_conf="${config.xdg.configHome}/kicad/${kicadVersion}/pcbnew.json"
+      jq '.appearance.color_theme = "${config.home.homeDirectory}/${kicadThemeDir}/${kicadPcbNewTheme}/${kicadPcbNewTheme}.json"' $pcbnew_conf | sponge $pcbnew_conf
+    '';
+  });
 
   home.packages = [
     myKicad
