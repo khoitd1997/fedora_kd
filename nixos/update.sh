8d249b08756fc56545f2149fbfc6224d7864d23f
khoitd1997 <khoidinhtrinh@gmail.com>
2025-01-03 21:16:30 -0800

    Bumped

--- nixos/update.sh
+++ nixos/update.sh
@@ -5,11 +5,4 @@ set -e
 script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd "${script_dir}"
 
-release_version="23.11"
-
-sudo nix-channel --add https://nixos.org/channels/"nixos-${release_version}" nixos
-sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-${release_version}.tar.gz home-manager
-sudo nix-channel --add https://nixos.org/channels/nixos-unstable unstable
-
-sudo nix-channel --update
-./rebuild.sh
+nix flake lock --update-input nixpkgs --update-input nixpkgs-unstable --update-input home-manager
