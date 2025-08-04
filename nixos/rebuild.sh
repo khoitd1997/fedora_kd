2bf1ea5932c6f425762fc5928b7f670ef526018f
khoitd1997 <khoidinhtrinh@gmail.com>
2025-08-04 13:02:55 -0700

    fix

--- nixos/rebuild.sh
+++ nixos/rebuild.sh
@@ -3,6 +3,6 @@
 script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd "${script_dir}"
 
-cp /etc/nixos/hardware-configuration.nix .
+# cp /etc/nixos/hardware-configuration.nix .
 
 sudo nixos-rebuild switch --install-bootloader --flake .#nixos-kd
